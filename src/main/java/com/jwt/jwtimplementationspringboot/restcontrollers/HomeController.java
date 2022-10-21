91817c8fafc2ea936cbe8168e05eab948c5feedc
NitishSachdeva <nitishsachdeva39@gmail.com>
2022-10-21 14:00:05 +0530

    Home Controller class

--- src/main/java/com/jwt/jwtimplementationspringboot/restcontrollers/HomeController.java
+++ src/main/java/com/jwt/jwtimplementationspringboot/restcontrollers/HomeController.java
@@ -4,7 +4,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class Home {
+public class HomeController {
     @RequestMapping(value={"", "/", "/home"})
     public String welcome(){
         String text = "Welcome to the home page. ";
