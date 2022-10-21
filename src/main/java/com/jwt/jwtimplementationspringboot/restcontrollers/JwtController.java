03d42246fc883c78f55ba1ee5d1801ede026d362
NitishSachdeva <nitishsachdeva39@gmail.com>
2022-10-21 13:59:42 +0530

    Jwt Controller class

--- src/main/java/com/jwt/jwtimplementationspringboot/restcontrollers/JwtController.java
+++ src/main/java/com/jwt/jwtimplementationspringboot/restcontrollers/JwtController.java
@@ -1,10 +1,9 @@
 package com.jwt.jwtimplementationspringboot.restcontrollers;
 
-import com.jwt.jwtimplementationspringboot.model.JWTRequest;
-import com.jwt.jwtimplementationspringboot.model.JWTResponse;
+import com.jwt.jwtimplementationspringboot.model.JwtRequest;
+import com.jwt.jwtimplementationspringboot.model.JwtResponse;
 import com.jwt.jwtimplementationspringboot.services.CustomUserDetailsService;
 import com.jwt.jwtimplementationspringboot.utility.JwtUtil;
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -12,12 +11,13 @@ import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.DisabledException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class JWTController {
+public class JwtController {
     @Autowired
     private AuthenticationManager authenticationManager;
     @Autowired
@@ -26,13 +26,13 @@ public class JWTController {
     private CustomUserDetailsService customUserDetailsService;
 
     @RequestMapping(value = "/token", method = RequestMethod.POST)
-    public ResponseEntity<?> generateToken(@RequestBody JWTRequest jwtRequest) throws Exception {
+    public ResponseEntity<?> generateToken(@RequestBody JwtRequest jwtRequest) throws Exception {
         System.out.println(jwtRequest);
         authenticate(jwtRequest.getUsername(), jwtRequest.getPassword());
         final UserDetails userDetails = this.customUserDetailsService.loadUserByUsername(jwtRequest.getUsername());
         final String token = this.jwtUtil.generateToken(userDetails);
         System.out.println("JWT TOKEN IS -->" + token);
-        return ResponseEntity.ok(new JWTResponse(token));
+        return ResponseEntity.ok(new JwtResponse(token));
     }
 
     private void authenticate(String username, String password) throws Exception {
