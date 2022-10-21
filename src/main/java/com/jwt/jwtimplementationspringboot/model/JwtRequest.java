656d5fff866c5cdc705b2efddf1b985fd6281a05
NitishSachdeva <nitishsachdeva39@gmail.com>
2022-10-21 14:08:32 +0530

    Lombok implementation

--- src/main/java/com/jwt/jwtimplementationspringboot/model/JwtRequest.java
+++ src/main/java/com/jwt/jwtimplementationspringboot/model/JwtRequest.java
@@ -3,7 +3,6 @@ package com.jwt.jwtimplementationspringboot.model;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 
 @Data
 @AllArgsConstructor
